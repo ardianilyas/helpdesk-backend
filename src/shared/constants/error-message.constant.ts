fe52d2e1e60675d0c146c1e533dba64c0eae65f1
ardianilyas <ardianilyas@gmail.com>
2026-09-23 01:13:29 +0700

    feat: add error message and test constants

---
+++ src/shared/constants/error-message.constant.ts
@@ -0,0 +1,9 @@
+export const ERROR_MESSAGE = {
+  UNAUTHORIZED: "Unauthorized",
+  FORBIDDEN: "Forbidden",
+  BAD_REQUEST: "Bad Request",
+  VALIDATION_ERROR: "Validation Error",
+  NOT_FOUND: "Not Found",
+  CONFLICT: "Conflict",
+  INTERNAL_SERVER_ERROR: "Internal Server Error"
+}
